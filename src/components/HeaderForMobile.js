73a927b8573ecbe953648172294c4cef6f415f78
NiranDx <niran3688@gmail.com>
2025-01-14 21:38:49 +0700

    update func search for mobile

--- src/components/HeaderForMobile.js
+++ src/components/HeaderForMobile.js
@@ -1,32 +1,55 @@
 import { HeartOutlined, SearchOutlined } from '@ant-design/icons';
-import { Input, Layout } from 'antd';
-import React from 'react';
+import { Drawer, Input, Layout } from 'antd';
+import React, { useRef, useState } from 'react';
+import SearchList from './SearchList';
 
-const { Header, Content, Footer } = Layout;
+const { Header } = Layout;
 
 const HeaderForMobile = () => {
+  const [open, setOpen] = useState(false);
+  const [inputSearch, setInputSearch] = useState('');
+  const inputRef = useRef(null);
+
+  const handleInputClick = () => {
+    setOpen(true);
+    setTimeout(() => {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, 0);
+  };
+
+  const handleInputChange = (e) => {
+    setInputSearch(e.target.value);
+  };
+
+  const handleCloseDrawer = () => {
+    setOpen(false);
+  };
+
   return (
     <Header
       style={{
         background: '#fff',
         position: 'sticky',
         top: 0,
-        left: 0,
-        bottom: 0,
-        zIndex: 10,
+        zIndex: 100,
         padding: '10px 20px',
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
-        width: '100%'
       }}
     >
       <h2 style={{ margin: 0, fontWeight: 'bold' }}>Instagram</h2>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <Input
+          ref={inputRef}
           placeholder="Search"
           prefix={<SearchOutlined />}
+          onClick={handleInputClick}
+          // onChange={handleInputChange}
+          onPressEnter={handleInputChange}
           style={{
             width: '200px',
             borderRadius: '10px',
@@ -36,6 +59,16 @@ const HeaderForMobile = () => {
         />
         <HeartOutlined style={{ fontSize: '20px', color: '#333' }} />
       </div>
+
+      <Drawer
+        placement="top"
+        onClose={handleCloseDrawer}
+        closeIcon={false}
+        open={open}
+        rootStyle={{ top: '64px', zIndex: '90' }}
+      >
+        <SearchList isShowSearch={false} search={inputSearch} />
+      </Drawer>
     </Header>
   );
 };
--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,80 +1,97 @@
 
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Button, Input, Typography } from 'antd';
-import React, { useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FetchQlUser } from '../api/getUser';
-import { AuthContext } from '../App';
 
 
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
-const SearchList = ({ name = "Nick", isFollow = false }) => {
-    // const { users: data } = useContext(AuthContext);
+const SearchList = ({ isShowSearch = true, search = '' }) => {
     const [inputSearch, setInputSearch] = useState('')
-    const { data: resData = [] } = FetchQlUser(undefined,null,inputSearch)
+    const [result, setResult] = useState([])
+    
+    const { data: resData = [] } = FetchQlUser(null, null, inputSearch)
     const handleChange = (e) => {
         setInputSearch(e.target.value)
     }
+
+    useEffect(() => {
+        setInputSearch(search)
+    }, [search])
+
+    useEffect(() => {
+        setResult(resData)
+    },[resData?.length])
+
     return (
         <>
-            <div>
-                <Input
-                    placeholder="Search..."
-                    allowClear
-                    onChange={handleChange}
-                />
-            </div>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px 0' }}>
+            {
+                isShowSearch && (
+                    <div>
+                        <Input
+                            placeholder="Search..."
+                            allowClear
+                            // onChange={handleChange}
+                            onPressEnter={handleChange}
+                        />
+                    </div>
+                )
+            }
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: !isShowSearch ? '0 0 20px 0' :'20px 0' }}>
                 <Text strong level={5}>Recent</Text>
                 <Button>Creal all</Button>
             </div>
-            {resData.length === 0 ? <div style={{ textAlign: 'center', alignContent: 'center', minHeight: '80%'}}>No recent searches</div> : resData.map((item, index) => {
-                return (
-                    <div key={`item-${index}`}
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            padding: '4px'
-                        }}
-                    >
-                        <div
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'flex-start',
-                                alignItems: 'center',
-                                gap: '16px',
-                            }}
-                        >
-                            <Avatar
-                                src={item.avatar.large}
-                                style={{
-                                    width: '40px',
-                                    height: '40px',
-                                    aspectRatio: '1 / 1',
-                                }}
-                                icon={<UserOutlined />}
-                            />
-                            <div
+            {
+                result.length === 0 ? <div style={{ textAlign: 'center', alignContent: 'center', minHeight: '80%' }}>No recent searches</div>
+                    :
+                    result.map((item, index) => {
+                        return (
+                            <div key={`item-${index}`}
                                 style={{
                                     display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'flex-start',
-                                    alignItems: 'flex-start',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    padding: '4px'
                                 }}
                             >
-                                <Text strong>{item.name}</Text>
-                                <Text>{`Followed by ${name}`}</Text>
-                            </div>
-                        </div>
-                        {!item.isFollowing &&
-                            <>
-                                <Button type="text">Follow</Button>
-                            </>
-                        }
-                    </div >
-                );
-            })}
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        justifyContent: 'flex-start',
+                                        alignItems: 'center',
+                                        gap: '16px',
+                                    }}
+                                >
+                                    <Avatar
+                                        src={item.avatar.large}
+                                        style={{
+                                            width: '40px',
+                                            height: '40px',
+                                            aspectRatio: '1 / 1',
+                                        }}
+                                        icon={<UserOutlined />}
+                                    />
+                                    <div
+                                        style={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: 'flex-start',
+                                            alignItems: 'flex-start',
+                                        }}
+                                    >
+                                        <Text strong>{item.name}</Text>
+                                        <Text>{`Followed by Nick`}</Text>
+                                    </div>
+                                </div>
+                                {!item.isFollowing &&
+                                    <>
+                                        <Button type="text">Follow</Button>
+                                    </>
+                                }
+                            </div >
+                        );
+                    })}
         </>
     )
 };
